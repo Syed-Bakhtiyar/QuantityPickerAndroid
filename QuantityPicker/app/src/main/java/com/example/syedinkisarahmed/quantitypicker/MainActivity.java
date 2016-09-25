ababf9ffae60ec40b8973dd7f8514d53a7eea36f
Syedbakhtiyar <Syedbakhtiyar120@gmail.com>
2016-09-25 09:20:21 +0500

    QuantityPicker

---
+++ QuantityPicker/app/src/main/java/com/example/syedinkisarahmed/quantitypicker/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.syedinkisarahmed.quantitypicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int quantity=0;
+    boolean a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public void increment(View v){
+        quantity = quantity+1;
+        display(quantity);
+    }
+
+
+
+    public void decrement (View v){
+        quantity = quantity-1;
+        display(quantity);
+    }
+
+
+
+    public void display(int x){
+        TextView txt = (TextView) findViewById(R.id.txt1);
+        txt.setText(x+"");
+
+
+    }
+
+
+
+
+    public void order(View v){
+        TextView txt = (TextView) findViewById(R.id.txt2);
+
+        CheckBox bx =(CheckBox) findViewById(R.id.chk);
+        a=bx.isChecked();
+        txt.setText("Name: Lyla the Labyrinth\nAdd whipped Cream? "+a+"\nQuantity: "+quantity+"\nTotal: $"+(quantity*5)+"\nThank you!");
+    }
+}
